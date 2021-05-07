2108b08d5cf293ee8e3aa6c70153c0a5f1f866bf
klequis <klequis@gmail.com>
2021-05-07 12:12:11 -0700

    dropzone added, start testing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 /* ORIGINAL */
+import { useState } from "react";
 import { Grid, Article, Box } from "./appStyles";
 import { accounts } from "./accounts";
+import { useDropzone } from "react-dropzone";
+import * as R from "ramda";
+import { DropZone } from "./DropZone";
+import { groupFiles } from "./groupFiles";
+import { customFileGetter } from "./customFileGetter";
 
-const Account = ({ account }) => {
+const Account = ({ account, getRootProps, getInputProps }) => {
   console.log("account", account);
   return (
     <Article>
       <h1>
         {account.acctName} (x{account.acctNumber})
       </h1>
-      <Box />
+      <DropZone
+        getRootProps={getRootProps}
+        getInputProps={getInputProps}
+        account={account}
+      />
       <p>
         This is the best computer money can buy, if you don’t have much money.
       </p>
@@ -18,10 +28,34 @@ const Account = ({ account }) => {
 };
 
 function App() {
+  const [_files, _setFiles] = useState({ accepted: [], rejected: [] });
+  const _onDrop = (acceptedFiles) => {
+    // one -->
+    // _setFiles(groupFiles(acceptedFiles))
+    // <--
+
+    const { accepted, rejected } = groupFiles(acceptedFiles);
+    // console.log('accepted', accepted)
+    // console.log('_files.accepted', _files.accepted)
+    _setFiles({
+      accepted: R.flatten([accepted, _files.accepted]),
+      rejected: R.flatten([rejected, _files.rejected]),
+    });
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop: _onDrop,
+    getFilesFromEvent: (event) => customFileGetter(event),
+  });
+
   return (
     <Grid>
       {accounts.map((a) => (
-        <Account account={a} />
+        <Account
+          account={a}
+          getRootProps={getRootProps}
+          getInputProps={getInputProps}
+        />
       ))}
     </Grid>
   );
---
+++ src/customFileGetter.js
@@ -0,0 +1,29 @@
+import * as R from "ramda";
+/**
+ *
+ * @param {object} file a File object
+ * @returns {object} a File object
+ */
+const updateAcceptProp = (file) => {
+  const filename = file.name;
+  const ext = filename.substr(filename.lastIndexOf(".") + 1);
+
+  Object.defineProperty(file, "accept", {
+    value: ext === "csv" ? true : false,
+  });
+  return file;
+};
+
+/**
+ *
+ * @param {event} event fileDrop event
+ * @returns {Array} array of accepted files
+ */
+export async function customFileGetter(event) {
+  // console.log('event', event)
+  const fileList = event.dataTransfer
+    ? event.dataTransfer.files
+    : event.target.files;
+  // console.log('fileList', fileList)
+  return R.map(updateAcceptProp, fileList);
+}
