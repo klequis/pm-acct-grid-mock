9b32a7b44303997f96e568c5d1a0de0830af3604
klequis <klequis@gmail.com>
2021-05-13 10:03:14 -0700

    implemented sample code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
-// import { useState } from "react";
-import { Grid } from './styles'
-// import { accounts } from "./accounts";
-// import Account from "./Account";
-import { Mock } from './Mock'
+import React, { useMemo } from 'react'
+import { useDropzone } from 'react-dropzone'
 
-function App() {
-  // const [_files, _setFiles] = useState({ accepted: [], rejected: [] });
+const baseStyle = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '20px',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: '#eeeeee',
+  borderStyle: 'dashed',
+  backgroundColor: '#fafafa',
+  color: '#bdbdbd',
+  outline: 'none',
+  transition: 'border .24s ease-in-out'
+}
 
-  // console.log(`_files`, _files);
+const activeStyle = {
+  borderColor: '#2196f3'
+}
 
-  return <Mock />
+const acceptStyle = {
+  borderColor: '#00e676'
+}
+
+const rejectStyle = {
+  borderColor: '#ff1744'
+}
+
+export const App = (props) => {
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject
+  } = useDropzone({ accept: '.csv' })
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {})
+    }),
+    [isDragActive, isDragReject, isDragAccept]
+  )
+
+  return (
+    <div className="container">
+      <div {...getRootProps({ style })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+    </div>
+  )
 }
 
 export default App
